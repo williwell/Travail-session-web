473420f6c7e77c0cd5eeb096a2e5e0c066fb3f6c
Glackssel <glackssel@gmail.com>
2020-10-26 14:10:36 -0400

    Création de différentes pages php et css
    Connexion.php
    Livres.php
    RechercheC.php
    RechercheD.php
    Livres.css
    Recherche.css

---
+++ Projet session web/Connexion.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>Vente de livres - Connexion</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="CSS/Connexion.css"/>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    </head>
+    <body>
+        <div id="Entete">
+            <div id="categorie">
+                <h1>Vente de livres</h1>
+            </div>
+            <div id="soustitre">
+                <div>
+                    <h3 id=soustitre>Connexion</h3>
+                </div>
+            </div>
+        </div>
+        <div id="Menu">
+        <a href="Recherche.php" class="Menu" id="Recherche">
+            <div>
+                <h3>Recherche</h3>
+            </div>
+        </a>
+        <a href="Livres.php" class="Menu" id="Livres">
+            <div>
+                
+                <h3>Mes livres</h3>
+            </div>
+        </a>
+        <a href="Connexion.php" class="Menu" id="Connexion">
+            <div>
+                <h3>Connexion</h3>
+            </div>
+        </a>
+                
+        </div>
+        <div id="Corps">
+        
+        </div>
+    </body>
+</html>
