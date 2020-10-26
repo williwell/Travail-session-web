287744dbb9c093b5ce5834302480c150966ec277
Glackssel <glackssel@gmail.com>
2020-10-26 15:14:49 -0400

    Ajout Compte

---
+++ Projet session web/Compte.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>Vente de livres - Connexion</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="CSS/Compte.css"/>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    </head>
+    <body>
+        <div id="Entete">
+            <div id="categorie">
+                <h1>Vente de livres</h1>
+            </div>
+            <div id="soustitre">
+                <div>
+                    <h3 id=soustitre>Compte</h3>
+                </div>
+            </div>
+        </div>
+        <div id="Menu">
+        <a href="RechercheC.php" class="Menu" id="Recherche">
+            <div>
+                <h3>Recherche</h3>
+            </div>
+        </a>
+        <a href="Livres.php" class="Menu" id="Livres">
+            <div>
+                <h3>Mes livres</h3>
+            </div>
+        </a>
+        <a href="Compte.php" class="Menu" id="Compte">
+            <div>
+                <h3>Mon compte</h3>
+            </div>
+        </a>
+                
+        </div>
+        <div id="Corps">
+        
+        </div>
+    </body>
+</html>
